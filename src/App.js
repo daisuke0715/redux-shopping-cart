89bbc9ad3fa6cab61aa6174615ea799aa5db5a16
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-03-20 19:07:59 +0900

    cartコンポーネントを作成してreducerの呼び出しをとともに実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { calculateTotals } from './features/cart/CartSlice';
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
---
+++ src/components/CartContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CartItem from './CartItem';
+import { clearCart } from '../features/cart/CartSlice';
+
+const CartContainer = () => {
+  const dispatch = useDispatch();
+  const { amount, cartItems, totalPrice } = useSelector((store) => store.cart);
+  return (
+      <section className='cart'>
+        <header>
+          <h2>買い物かご</h2>
+          { amount < 1 && (
+          <h4 className='empty-cart'>何も入っていません...</h4>
+          )}
+          <div>
+            {cartItems.map((item) => {
+              return <CartItem key={item.id} {...item} />;
+            })}
+          </div>
+        </header>
+        <footer>
+          <hr />
+          <div className='cart-total-price'>
+            <h4>
+              合計 <span>{totalPrice}円</span>
+            </h4>
+          </div>
+          <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>全削除</button> 
+        </footer>
+      </section>
+  );
+}
+
+export default CartContainer;
